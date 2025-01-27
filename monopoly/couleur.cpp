de1cde2f18dac3a5a03cdde7ecf5bea3d87c527b
Bastien <159130673+assaiz@users.noreply.github.com>
2025-01-27 04:52:51 +0000

    commentaire du code en doxyfile

--- monopoly/couleur.cpp
+++ monopoly/couleur.cpp
@@ -1,43 +1,75 @@
+/**
+ * @file couleur.cpp
+ * @brief Implémentation des méthodes de la classe Couleur
+ * @version 0.1
+ * @date 2025-01-26
+ * 
+ * @author PEAN Louis
+ * @author DARGERE Lucas
+ * @author LAVAUX Bastien
+ * @copyright Copyright (c) 2025
+ */
+
 #include "couleur.hpp"
 
-
-Couleur::Couleur(vector<Actif*> rue){
-    _rue = rue ;
+/**
+ * @brief Constructeur de la classe Couleur
+ * 
+ * Initialise un objet Couleur avec un vecteur de cases (Actif).
+ * 
+ * @param rue Vecteur de pointeurs vers les cases associées à cette couleur.
+ */
+Couleur::Couleur(vector<Actif*> rue) {
+    _rue = rue;
 }
 
-vector<Actif*> Couleur::listeConstructible(Joueur* joueur){
-    if(joueur == _joueur)
-    {
+/**
+ * @brief Retourne la liste des cases constructibles pour un joueur donné.
+ * 
+ * Si le joueur passé en paramètre est le propriétaire de toutes les cases
+ * de cette couleur, retourne la liste complète des cases. Sinon, retourne 
+ * un vecteur contenant `nullptr`.
+ * 
+ * @param joueur Pointeur vers le joueur dont les droits sont vérifiés.
+ * @return vector<Actif*> Liste des cases constructibles ou un vecteur contenant `nullptr` si non valide.
+ */
+vector<Actif*> Couleur::listeConstructible(Joueur* joueur) {
+    if (joueur == _joueur) {
         return _rue;
-    }
-    else
-    {
+    } else {
         return vector<Actif*>{nullptr};
     }
 }
 
-void Couleur::miseAJour(){
+/**
+ * @brief Met à jour l'état des cases de cette couleur.
+ * 
+ * Vérifie si toutes les cases de la couleur ont le même propriétaire. 
+ * Si c'est le cas, elles deviennent constructibles. Sinon, elles restent non constructibles.
+ */
+void Couleur::miseAJour() {
     if (_rue.empty()) {
         cout << "Aucune case dans la rue." << endl;
         return;
     }
 
     // Obtenir le propriétaire de la première case
-    Joueur* premierProprietaire = _rue.front()->getProprio(); // Remplacez `proprietaire` par le champ réel
+    Joueur* premierProprietaire = _rue.front()->getProprio();
 
     // Vérifier si tous les propriétaires sont les mêmes
     bool tousIdentiques = true;
     for (Actif* c : _rue) {
-        if (c->getProprio() != premierProprietaire) { // Comparaison avec le premier propriétaire
+        if (c->getProprio() != premierProprietaire) {
             tousIdentiques = false;
             break;
         }
     }
 
+    // Mettre à jour l'état constructible si tous les propriétaires sont identiques
     if (tousIdentiques) {
         for (Actif* c : _rue) {
             c->setConstructible(1);
+            _joueur == c->getProprio();
         }
     } 
 }
-
--- monopoly/gare.cpp
+++ monopoly/gare.cpp
@@ -1,17 +1,61 @@
+/**
+ * @file gare.cpp
+ * @brief Implémentation des méthodes de la classe Gare
+ * @version 0.1
+ * @date 2025-01-26
+ * 
+ * @author PEAN Louis
+ * @author DARGERE Lucas
+ * @author LAVAUX Bastien
+ * @copyright Copyright (c) 2025
+ */
+
 #include "gare.hpp"
 
+/**
+ * @brief Tableau statique définissant le loyer en fonction du nombre de gares possédées.
+ * 
+ * Indices :
+ * - [0] : Aucun loyer (0 gare possédée)
+ * - [1] : Loyer pour 1 gare (-25)
+ * - [2] : Loyer pour 2 gares (-50)
+ * - [3] : Loyer pour 3 gares (-100)
+ * - [4] : Loyer pour 4 gares (-200)
+ */
 const int Gare::_loyer[5] = {0, -25, -50, -100, -200};
 
-Gare::Gare(string nom): Actif(nom){}
+/**
+ * @brief Constructeur de la classe Gare
+ * 
+ * Initialise une gare avec son nom.
+ * 
+ * @param nom Nom de la gare.
+ */
+Gare::Gare(string nom) : Actif(nom) {}
 
-int Gare::tarif(Joueur* joueur){
-    int nbGare = _proprietaire->getNbGare();
+/**
+ * @brief Calcule le tarif de location pour un joueur.
+ * 
+ * Le tarif est basé sur le nombre de gares possédées par le propriétaire de cette gare.
+ * 
+ * @param joueur Pointeur vers le joueur pour lequel le tarif est calculé.
+ * @return int Montant du loyer dû par le joueur.
+ */
+int Gare::tarif(Joueur* joueur) {
+    int nbGare = _proprietaire->getNbGare(); // Nombre de gares possédées par le propriétaire
     return _loyer[nbGare];
 }
 
-
-void Gare::achat(Joueur* joueur){
-    _proprietaire=joueur;
-    joueur->setSolde(-200);
-    _proprietaire->addGare();
+/**
+ * @brief Gère l'achat d'une gare par un joueur.
+ * 
+ * Associe le joueur comme propriétaire de la gare, retire 200 unités de son solde
+ * et met à jour le nombre de gares possédées par ce joueur.
+ * 
+ * @param joueur Pointeur vers le joueur effectuant l'achat.
+ */
+void Gare::achat(Joueur* joueur) {
+    _proprietaire = joueur;           // Attribuer le joueur comme propriétaire
+    joueur->setSolde(-200);           // Réduire le solde du joueur de 200 unités
+    _proprietaire->addGare();         // Incrémenter le nombre de gares possédées par le joueur
 }
